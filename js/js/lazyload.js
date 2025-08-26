7265a41119fc2e55fb15386c7d318c98e59228db
pyahermanas-ops <pyahermanas@gmail.com>
2025-08-26 16:43:48 -0300

    Create lazyload.js

---
+++ js/js/lazyload.js
@@ -0,0 +1,19 @@
+// Lazy load de imágenes
+document.addEventListener("DOMContentLoaded", function() {
+    const lazyImages = document.querySelectorAll('img.lazy');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                const img = entry.target;
+                img.src = img.dataset.src;
+                img.classList.remove('lazy');
+                observer.unobserve(img);
+            }
+        });
+    });
+
+    lazyImages.forEach(img => {
+        observer.observe(img);
+    });
+});
